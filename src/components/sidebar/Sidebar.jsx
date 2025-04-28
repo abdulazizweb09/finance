cc9a9180af1b79b77389f160b3f79e2bccdb1af9
abdulaziz055 <abdulazizabdurahimov519>
2025-04-28 14:50:54 +0500

    refactor: Add lazy loading for sidebar images

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,9 +4,11 @@ import inter from "../../imgs/inter.svg";
 import bud from "../../imgs/bud.svg";
 import collect from "../../imgs/collect.svg";
 import chek from "../../imgs/chek.svg";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Sidebar({ test }) {
+  let [shownavbar, setshownavbar] = useState(true);
   let navigate=useNavigate()
   function home() {
     navigate('/')
@@ -23,47 +25,101 @@ function Sidebar({ test }) {
   function bill() {
     navigate('/bills')
   }
+  let location = useLocation(); 
+
+  useEffect(() => {
+    setshownavbar(false); 
+  }, [location.pathname]);
   return (
-    <div className={styles.sidebar}>
+    <div className={shownavbar?styles.sidebar:styles.minSidebar}>
       <div className={styles.logo}>finance</div>
 
-      <div className={styles.menu}>
-        <div
-          onClick={home}
-          className={test == "overview" ? styles.alt : styles.menuItem}
-        >
-          <img src={hom} alt="" />
-          <span>Overview</span>
-        </div>
+      {!shownavbar && (
+        <div className={`styles.menu ${styles.min}`}>
+          <div
+            onClick={home}
+            className={test == "overview" ? styles.alt : styles.menuItem}
+          >
+            <img src={hom} alt="" />
+          </div>
 
-        <div
-          onClick={tra}
-          className={test == "transactions" ? styles.alt : styles.menuItem}
-        >
-          <img src={inter} alt="" />
-          <span>Transactions</span>
-        </div>
+          <div
+            onClick={tra}
+            className={test == "transactions" ? styles.alt : styles.menuItem}
+          >
+            <img src={inter} alt="" />
+          </div>
 
-        <div
-          onClick={budget}
-          className={test == "budgets" ? styles.alt : styles.menuItem}
-        >
-          <img src={bud} alt="" />
-          <span>Budgets</span>
-        </div>
+          <div
+            onClick={budget}
+            className={test == "budgets" ? styles.alt : styles.menuItem}
+          >
+            <img src={bud} alt="" />
+          </div>
 
-        <div onClick={post} className={test == "pots" ? styles.alt : styles.menuItem}>
-          <img src={collect} alt="" />
-          <span>Pots</span>
-        </div>
+          <div
+            onClick={post}
+            className={test == "pots" ? styles.alt : styles.menuItem}
+          >
+            <img src={collect} alt="" />
+          </div>
 
-        <div onClick={bill} className={test === "bills" ? styles.alt : styles.menuItem}>
-          <img src={chek} alt="" />
-          <span>Recurring bills</span>
+          <div
+            onClick={bill}
+            className={test === "bills" ? styles.alt : styles.menuItem}
+          >
+            <img src={chek} alt="" />
+          </div>
         </div>
-      </div>
+      )}
+      {shownavbar && (
+        <div className={styles.menu}>
+          <div
+            onClick={home}
+            className={test == "overview" ? styles.alt : styles.menuItem}
+          >
+            <img src={hom} alt="" />
+            <span>Overview</span>
+          </div>
 
-      <div className={styles.minimize}>
+          <div
+            onClick={tra}
+            className={test == "transactions" ? styles.alt : styles.menuItem}
+          >
+            <img src={inter} alt="" />
+            <span>Transactions</span>
+          </div>
+
+          <div
+            onClick={budget}
+            className={test == "budgets" ? styles.alt : styles.menuItem}
+          >
+            <img src={bud} alt="" />
+            <span>Budgets</span>
+          </div>
+
+          <div
+            onClick={post}
+            className={test == "pots" ? styles.alt : styles.menuItem}
+          >
+            <img src={collect} alt="" />
+            <span>Pots</span>
+          </div>
+
+          <div
+            onClick={bill}
+            className={test === "bills" ? styles.alt : styles.menuItem}
+          >
+            <img src={chek} alt="" />
+            <span>Recurring bills</span>
+          </div>
+        </div>
+      )}
+
+      <div
+        onClick={() => setshownavbar(!shownavbar)}
+        className={styles.minimize}
+      >
         <span>Minimize Menu</span>
       </div>
     </div>
